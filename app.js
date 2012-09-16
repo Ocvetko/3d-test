fc516742dc6098e86b50052633bb9219a501b416
irony <cl@iteam.se>
2012-09-16 21:40:07 +0200

    Adjustments finishing touches

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , gzippo = require('gzippo')
 
 var app = module.exports = express.createServer();
 
@@ -16,7 +17,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(gzippo.staticGzip(__dirname + '/public'));
 });
 
 app.configure('development', function(){
