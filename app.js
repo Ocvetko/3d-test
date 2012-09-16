6ea8344256eccae24b522a82b12c37ccafe6e74a
irony <cl@iteam.se>
2012-09-16 21:52:01 +0200

    Adjustments finishing touches

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , gzippo = require('gzippo')
+  , connect = require('connect')
 
 var app = module.exports = express.createServer();
 
@@ -17,7 +17,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(gzippo.staticGzip(__dirname + '/public'));
+  app.use(connect.compress());
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
